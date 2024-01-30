4a4e8709f1d049bf272c41cb7716add4a1cb3b7a
badgerson <mail@badgerson.com>
2024-01-30 11:38:04 +0100

    Revert "do not use hledger -f argument"
    This reverts commit 5b1e372934a1b1b34e695c91d763947f66922731.

--- lua/cmp_hledger/init.lua
+++ lua/cmp_hledger/init.lua
@@ -33,8 +33,8 @@ local split = function(str, sep)
   return t
 end
 
-local get_items = function()
-  local openPop = assert(io.popen(vim.b.hledger_bin .. ' accounts'))
+local get_items = function(account_path)
+  local openPop = assert(io.popen(vim.b.hledger_bin .. ' accounts -f ' .. account_path))
   local output = openPop:read('*all')
   openPop:close()
   local t = split(output, "\n")
@@ -67,8 +67,9 @@ source.complete = function(self, request, callback)
     callback()
     return
   end
+  local account_path = vim.api.nvim_buf_get_name(0)
   if not self.items then
-    self.items = get_items()
+    self.items = get_items(account_path)
   end
 
   local prefix_mode = false
